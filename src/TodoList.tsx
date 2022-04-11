46b929d3d5c91ad670effaed92a6f30c43d731e1
JaejinHwang <hjj4756@gmail.com>
2022-04-12 02:19:30 +0900

    form validation

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,13 +1,46 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import { SubmitHandler, useForm } from "react-hook-form";
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0px;
+`;
+
+interface IForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  passwordConfirm: string;
+}
 
 const TodoList = () => {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>();
+  const onSubmit: SubmitHandler<IForm> = (data) => {
+    console.log(data);
+  };
+  console.log(errors);
   return (
     <>
-      <form>
-        <input placeholder="Email" {...register("email")} />
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Email"
+          {...register("email", {
+            required: "Email Required",
+            pattern: {
+              value:
+                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+              message: "Email pattern invalid",
+            },
+          })}
+        />
+        {errors?.email?.message}
         <input placeholder="First Name" {...register("firstName")} />
         <input placeholder="Last Name" {...register("lastName")} />
         <input placeholder="Password" {...register("password")} />
@@ -16,7 +49,7 @@ const TodoList = () => {
           {...register("passwordConfirm")}
         />
         <button>Add</button>
-      </form>
+      </Form>
     </>
   );
 };
