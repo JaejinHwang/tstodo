c0a8bdd080daef3fe425dca5e71837f1a75f6500
JaejinHwang <hjj4756@gmail.com>
2022-04-13 02:05:26 +0900

    connect button with category state

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,7 @@
 import { ITodo } from "./Atoms";
 import styled from "styled-components";
+import { useRecoilState } from "recoil";
+import { todosAtom } from "./Atoms";
 
 const ItemContainer = styled.li`
   width: 100%;
@@ -12,11 +14,38 @@ const ItemContainer = styled.li`
 `;
 
 const TodoItem = ({ text, category, date }: ITodo) => {
+  const [todos, setTodos] = useRecoilState(todosAtom);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name: targetName },
+    } = event;
+    const targetIndex = todos.findIndex((todo) => todo.date === date);
+    const updatedTodo = { text, date, category: targetName as any };
+    setTodos((remainTodos) => [
+      ...remainTodos.slice(0, targetIndex),
+      updatedTodo,
+      ...remainTodos.slice(targetIndex + 1),
+    ]);
+  };
   return (
     <ItemContainer>
       <span>{text}</span>
       <span>{category}</span>
-      <button>start</button>
+      {category === "TO DO" && (
+        <button name="DOING" onClick={onClick}>
+          start
+        </button>
+      )}
+      {category === "DOING" && (
+        <>
+          <button name="TO DO" onClick={onClick}>
+            back
+          </button>
+          <button name="DONE" onClick={onClick}>
+            finish
+          </button>
+        </>
+      )}
     </ItemContainer>
   );
 };
